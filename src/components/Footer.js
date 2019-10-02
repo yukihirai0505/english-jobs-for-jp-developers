ef50fe6c9ff1289ed273b5c15bbdd9197f09eb64
Yuki Hirai <yukihirai0505@gmail.com>
2019-10-02 22:53:14 +0900

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -46,32 +46,11 @@ const Footer = () => (
     <List>
       <Item>
         <a
-          href="https://www.contentful.com/"
-          rel="nofollow noopener noreferrer"
-          target="_blank"
-        >
-          <img
-            src="https://images.ctfassets.net/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg"
-            style={{ width: '100px' }}
-            alt="Powered by Contentful"
-          />
-        </a>
-      </Item>
-      <Item>
-        <a
-          href="https://github.com/ryanwiemer/gatsby-starter-gcn"
+          href="https://github.com/yabaiwebyasan"
           target="_blank"
           rel="noopener noreferrer"
         >
-          gatsby-starter-gcn
-        </a>{' '}
-        by{' '}
-        <a
-          href="https://github.com/ryanwiemer"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          @ryanwiemer
+          @yabaiwebyasan
         </a>
       </Item>
     </List>
